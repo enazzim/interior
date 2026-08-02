5262dc94c25133a80f93c420b543e1ead76db516
enazzim <enazzim@users.noreply.github.com>
2026-08-03 01:38:44 +0900

    fix: set axios baseURL to empty string in Vite dev mode to use proxy to 8080

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,11 +4,12 @@ import axios from 'axios'
 import './index.css'
 import App from './App.tsx'
 
-// Electron 환경일 경우 로컬 포트 38080 고정, 웹 브라우저 배포 모드일 경우 EC2 백엔드 주소로 통신
 if ((window as any).electronAPI) {
   axios.defaults.baseURL = 'http://localhost:38080';
+} else if (import.meta.env.DEV) {
+  axios.defaults.baseURL = '';
 } else {
-  axios.defaults.baseURL = window.location.protocol + '//' + window.location.hostname + ':38080';
+  axios.defaults.baseURL = window.location.protocol + '//' + window.location.hostname + ':8080';
 }
 
 // Electron에서 네이티브 alert 및 confirm 창이 닫힌 후 키보드 포커스를 즉시 회복 (딜레이 0ms 화)
